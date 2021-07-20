12cc53e45ce67ecc51bf9ff75f91ecfa543fd232
Mykhailo Sobko <m.t.sobko@gmail.com>
2021-07-20 12:01:38 +0300

    Add multiple port detection

--- setter.py
+++ setter.py
@@ -1,18 +1,11 @@
 # Importing Libraries
 import serial
+import serial.tools.list_ports
 import time
 
-arduino = serial.Serial(port='/dev/ttyUSB1', baudrate=9600, timeout=.1, rtscts=False, dsrdtr=False)
-com = serial.Serial()
-com.port = '/dev/ttyUSB1'
-com.baudrate = 9600 
-com.timeout = .1
-com.setDTR(False)
-com.open()
-def write(date):
-    arduino.write(bytes('s'+date, 'utf-8'))
-    time.sleep(0.05)
-
-date = input("Enter time: ")
-value = write(date)
-
+for port in serial.tools.list_ports.comports():
+    if "Serial" in port.description:
+        arduino = serial.Serial(port=port.device, baudrate=9600, timeout=.1, rtscts=False, dsrdtr=False)
+        date = input("Enter time: ")
+        arduino.write(bytes('s'+date, 'utf-8'))
+        time.sleep(0.05)
